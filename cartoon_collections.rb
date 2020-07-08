0e40eba09161a88bbb507f7f1d1d4a1d88b604f8
Jzavier Timm <jztimm@gmail.com>
2020-07-08 16:59:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,15 +27,23 @@ def long_planeteer_calls(planeteer_calls)
   end
 end
 
+# def find_valid_calls(planeteer_calls)
+#   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+#   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
+#   # Return the first valid call found, or return nil if no valid calls are found
+  
+  
+#   planeteer_calls.find do |index|
+#     valid_calls.find do |calls|
+#       index == calls
+#     end
+#   end
+# end
+
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
-  # Return the first valid call found, or return nil if no valid calls are found
-  
-  
-  planeteer_calls.find do |index|
-    valid_calls.find do |calls|
-      index == calls
-    end
+  planeteer_calls.find do |element|
+    valid_calls.include?(element)
   end
 end
+
